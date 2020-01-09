9fde0f306e2d8bd23411cd3baf85e48af728629a
Kevin Corbin <kecorbin@users.noreply.github.com>
2020-01-09 16:35:58 -0600

    add demo recored

--- sample_record.tf
+++ sample_record.tf
@@ -7,3 +7,12 @@ resource "aws_route53_record" "sample_record" {
   ttl     = "300"
   records = ["5.5.5.5"]
 }
+
+
+resource "aws_route53_record" "sample_record" {
+  zone_id = "${aws_route53_zone.aws_sub_zone.0.zone_id}"
+  name    = "sample2"
+  type    = "A"
+  ttl     = "300"
+  records = ["5.5.5.6"]
+}
